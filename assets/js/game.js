641870555e89d6f4559340eeebfa1c09d620ec58
Your Name <lubezkibernard@Lubezkis-MacBook-Pro.local>
2022-03-13 17:45:15 -0400

    random-order feature update

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,12 +37,22 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
-  while (playerInfo.health > 0 && enemy.health > 0) {
-    if (fightOrSkip()) {
-        // if tru, leave fight by breaking loop
-        break;
-    }
+  // keep track of who goes first
+  var isPlayerTurn = true;
 
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
+    }
     // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     
@@ -79,6 +89,8 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn
 };
 
 // function to set name
